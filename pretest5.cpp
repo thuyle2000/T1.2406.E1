cdb21f08a023e32f8413626d5b7de92b07c0c26d
thuyle2000 <thuyle2000@gmail.com>
2024-07-29 18:01:56 +0700

    update

--- pretest5.cpp
+++ pretest5.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <math.h>
+
 void q1();
 void q2();
 int main()
@@ -34,13 +36,56 @@ int main()
             break;
         }
 
-        printf(" >>> press any key to continue ... ");
+        printf("\n\n >>> press any key to continue ... ");
         getch();
     }
 }
-
+/*
+Write a function, allows the user to input a positive integer number -
+after that check this number whelther is the amstrong number or not .
+ex: 153, 370, 371, 407, 1634, 8208, 9474,54748, 92727
+*/
 void q1()
 {
+    system("cls");
+
+    fseek(stdin, 0, 0);
+    int number, sum = 0, n = 0;
+    while (1 == 1)
+    {
+        printf(" >> vui long nhap so nguyen duong > 0: ");
+        scanf("%d", &number);
+        if (number > 0)
+            break;
+    }
+
+    // dem so luong chu so trong [number], dat vo bien [n]
+    n = 0;
+    int temp = number;
+    while (temp > 0)
+    {
+        temp = temp / 10;
+        n++;
+    }
+
+    // tinh tong luy thua (n) cua tung chu so
+    int digit = 0;
+    temp = number;
+    for (int i = 0; i < n; i++)
+    {
+        digit = temp % 10;
+        temp = temp /10;
+        sum += (int)pow(digit, n); // = digit^n
+    }
+
+    if (sum == number)
+    {
+        printf(" >> %d la so amstrong !\n", number);
+    }
+    else
+    {
+        printf(" >> %d KHONG PHAI LA so amstrong !\n", number);
+    }
 }
 
 struct DOCTOR
